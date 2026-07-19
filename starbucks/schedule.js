7f14251d79afb6144da34844255bbacbfcd8c32b
austin <167563184+austinkden@users.noreply.github.com>
2026-07-19 13:50:42 -0600

    Update schedule.js

--- starbucks/schedule.js
+++ starbucks/schedule.js
@@ -20,16 +20,17 @@ const STARBUCKS_SCHEDULE = {
     "2026-07-15": { start: "13:00", end: "17:30", notes: "" },
     "2026-07-16": { start: "15:15", end: "19:15", notes: "" },
     "2026-07-18": { start: "07:45", end: "12:30", notes: "" },
+    "2026-07-19": { start: "12:00", end: "16:00", notes: "Same-Day Covered" }
     "2026-07-21": { start: "10:00", end: "17:00", notes: "" },
     "2026-07-22": { start: "12:00", end: "16:00", notes: "" },
     "2026-07-23": { start: "10:45", end: "15:00", notes: "" },
     "2026-07-24": { start: "10:45", end: "14:45", notes: "" },
     "2026-07-26": { start: "08:00", end: "12:45", notes: "" },
-    "2026-07-28": { start: "12:00", end: "20:30", notes: "Coverage" },
-    "2026-07-29": { start: "07:15", end: "12:00", notes: "Coverage" },
-    "2026-07-30": { start: "07:00", end: "13:30", notes: "Coverage" },
-    "2026-07-31": { start: "12:45", end: "19:15", notes: "Coverage" },
-    "2026-08-02": { start: "11:00", end: "18:30", notes: "Coverage" }
+    "2026-07-28": { start: "12:00", end: "20:30", notes: "" },
+    "2026-07-29": { start: "07:15", end: "12:00", notes: "" },
+    "2026-07-30": { start: "07:00", end: "13:30", notes: "" },
+    "2026-07-31": { start: "12:45", end: "19:15", notes: "" },
+    "2026-08-02": { start: "11:00", end: "18:30", notes: "" }
 };
 
 window.STARBUCKS_SCHEDULE = STARBUCKS_SCHEDULE;
